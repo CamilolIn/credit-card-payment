1d40a93a806b5500b85ca7195d0824510c71ed84
CamilolIn <43356871+CamilolIn@users.noreply.github.com>
2023-09-05 14:10:22 -0500

    theme dark light mode

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
+import { ColorMode, useMode } from "./theme";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import NavBar from "./views/navbar/NavBar";
 
 const App = () => {
+
+  const [theme, colorMode] = useMode();
+
   return (
-    <div className="App">
-      Hello
-    </div>
+    <ColorMode.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+
+        <div className="App">
+          <main className="content">
+            <NavBar />
+          </main>
+        </div>
+      </ThemeProvider>
+    </ColorMode.Provider>
   );
 }
 
---
+++ src/data/mockCreditCard.js
@@ -0,0 +1,8 @@
+export const mockDataCreditCards = [
+  {
+    name: "Camilo Lindarte",
+    creditNumber: 1234567812345678,
+    validThru: "07/2025",
+    cvc: 555
+  }
+]
